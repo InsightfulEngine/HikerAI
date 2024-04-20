7189c70d1fea8ec1486e3001eb690dee65c9447b
naa-7 <nalawad000@citymail.cuny.edu>
2024-04-20 13:10:59 -0400

    Feature[constants]: Add constants file

---
+++ frontend/constants/index.ts
@@ -0,0 +1,133 @@
+export const trailBorough = [
+  {
+    title: "Borough",
+    value: "",
+  },
+  {
+    title: "Bronx",
+    value: "Bronx",
+  },
+  {
+    title: "Brooklyn",
+    value: "Brooklyn",
+  },
+  {
+    title: "Manhattan",
+    value: "Manhattan",
+  },
+  {
+    title: "Queens",
+    value: "Queens",
+  },
+  {
+    title: "Staten Island",
+    value: "Staten Island",
+  },
+];
+
+export const trailType = [
+  {
+    title: "Type",
+    value: "",
+  },
+  {
+    title: "Loop",
+    value: "Loop",
+  },
+  {
+    title: "Out and Back",
+    value: "Out and Back",
+  },
+  {
+    title: "Point to Point",
+    value: "Point to Point",
+  },
+  {
+    title: "Lollipop",
+    value: "Lollipop",
+  },
+];
+
+export const trailLength = [
+  {
+    title: "Length",
+    value: "",
+  },
+  {
+    title: "Short",
+    value: "Short",
+  },
+  {
+    title: "Medium",
+    value: "Medium",
+  },
+  {
+    title: "Long",
+    value: "Long",
+  },
+];
+
+export const trailDifficulty = [
+  {
+    title: "Difficulty",
+    value: "",
+  },
+  {
+    title: "Easy",
+    value: "Easy",
+  },
+  {
+    title: "Moderate",
+    value: "Moderate",
+  },
+  {
+    title: "Hard",
+    value: "Hard",
+  },
+];
+
+export const trailRating = [
+  {
+    title: "Rating",
+    value: "",
+  },
+  {
+    title: "1 Star",
+    value: "1 Star",
+  },
+  {
+    title: "2 Stars",
+    value: "2 Stars",
+  },
+  {
+    title: "3 Stars",
+    value: "3 Stars",
+  },
+  {
+    title: "4 Stars",
+    value: "4 Stars",
+  },
+  {
+    title: "5 Stars",
+    value: "5 Stars",
+  },
+];
+
+export const trailNoise = [
+  {
+    title: "Noise",
+    value: "",
+  },
+  {
+    title: "Quiet",
+    value: "Quiet",
+  },
+  {
+    title: "Normal",
+    value: "Normal",
+  },
+  {
+    title: "Loud",
+    value: "Loud",
+  },
+];
